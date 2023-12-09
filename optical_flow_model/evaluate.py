7167076424f30c394b3f5bfa9fb587f12b0863fc
lamchonhang <joejoelam1112@gmail.com>
2023-12-09 11:41:56 +0800

    extract only the needed files from RAFT

---
+++ optical_flow_model/evaluate.py
@@ -0,0 +1,80 @@
+import sys
+sys.path.append('core')
+
+from PIL import Image
+import argparse
+import os
+import time
+import numpy as np
+import torch
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+
+import utils.dataset_kittiflow as dataset_kittiflow
+
+from models.raft import RAFT
+from utils.utils import InputPadder
+
+@torch.no_grad()
+def validate_kitti(model, iters=24):
+    """ Peform validation using the KITTI-2015 (train) split """
+    model.eval()
+    # val_dataset = datasets.KITTI(split='training')
+    val_dataset = dataset_kittiflow.KITTI(split='training')
+    
+    out_list, epe_list, fps_list = [], [], []
+    for val_id in range(len(val_dataset)):
+        image1, image2, flow_gt, valid_gt = val_dataset[val_id]
+        image1 = image1[None].cuda()
+        image2 = image2[None].cuda()
+
+        padder = InputPadder(image1.shape, mode='kitti')
+        image1, image2 = padder.pad(image1, image2)
+        
+        # inference
+        t1 = time.time()
+        flow_low, flow_pr = model(image1, image2, iters=iters, test_mode=True)
+        torch.cuda.synchronize()
+        t2 = time.time()
+        fps_list.append(1/(t2-t1))
+        flow = padder.unpad(flow_pr[0]).cpu()
+
+        epe = torch.sum((flow - flow_gt)**2, dim=0).sqrt()
+        mag = torch.sum(flow_gt**2, dim=0).sqrt()
+
+        epe = epe.view(-1)
+        mag = mag.view(-1)
+        val = valid_gt.view(-1) >= 0.5
+
+        out = ((epe > 3.0) & ((epe/mag) > 0.05)).float()
+        epe_list.append(epe[val].mean().item())
+        out_list.append(out[val].cpu().numpy())
+
+    epe_list = np.array(epe_list)
+    out_list = np.concatenate(out_list)
+
+    epe = np.mean(epe_list)
+    f1 = 100 * np.mean(out_list)
+    fps = np.mean(fps_list)
+    print("Validation KITTI: epe = %f, f1 = %f, avg fps = %f" % (epe, f1, fps))
+    return {'kitti-epe': epe, 'kitti-f1': f1}
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', help="restore checkpoint")
+    parser.add_argument('--small', action='store_true', help='use small model')
+    parser.add_argument('--mixed_precision', action='store_true', help='use mixed precision')
+    parser.add_argument('--iters', type=int, default=12)
+    args = parser.parse_args()
+
+    model = torch.nn.DataParallel(RAFT(args))
+    model.load_state_dict(torch.load(args.model))
+
+    model.cuda()
+    model.eval()
+
+    with torch.no_grad():
+        validate_kitti(model.module, iters=args.iters)
+
+
